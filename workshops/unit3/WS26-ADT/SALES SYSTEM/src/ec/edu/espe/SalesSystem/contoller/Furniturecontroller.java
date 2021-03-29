13d57d94897e94a7edcf9d6e343342816fc0cb78
Aulio Martin Lema Lema <75451518+Martinlema@users.noreply.github.com>
2021-03-28 19:43:36 -0500

    WS26-ADT

---
+++ workshops/unit3/WS26-ADT/SALES SYSTEM/src/ec/edu/espe/SalesSystem/contoller/Furniturecontroller.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.SalesSystem.contoller;
+
+import Utils.FileManager;
+import ec.edu.espe.SalesSystem.model.Furniture;
+
+/**
+ *
+ * @author martin lema
+ */
+public class Furniturecontroller {
+    
+    public void save(Furniture furniture){
+    String data=furniture.getCode() +"," + furniture.getPrice()+ ","+ furniture.getSize()+" ,"+ furniture.getColors()+","+ furniture.getBrand()+","+ furniture.isCondition();
+FileManager.save(data,"Furnitures");
+    
+    
+} 
+}
+
+ 
+
---
+++ workshops/unit3/WS26-ADT/SALES SYSTEM/src/ec/edu/espe/SalesSystem/model/Furniture.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.SalesSystem.model;
+
+/**
+ *
+ * @author martin lema
+ */
+public class Furniture {
+    private int code;
+    private int price;
+    private String size;
+    private String colors;
+    private String Brand;
+    private boolean condition;
+
+    public Furniture(int code, int price, String size, String colors, String Brand, boolean condition) {
+        this.code = code;
+        this.price = price;
+        this.size = size;
+        this.colors = colors;
+        this.Brand = Brand;
+        this.condition = condition;
+    }
+
+    /**
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the size
+     */
+    public String getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the colors
+     */
+    public String getColors() {
+        return colors;
+    }
+
+    /**
+     * @param colors the colors to set
+     */
+    public void setColors(String colors) {
+        this.colors = colors;
+    }
+
+    /**
+     * @return the Brand
+     */
+    public String getBrand() {
+        return Brand;
+    }
+
+    /**
+     * @param Brand the Brand to set
+     */
+    public void setBrand(String Brand) {
+        this.Brand = Brand;
+    }
+
+    /**
+     * @return the condition
+     */
+    public boolean isCondition() {
+        return condition;
+    }
+
+    /**
+     * @param condition the condition to set
+     */
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+}
+    
